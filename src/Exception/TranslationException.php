6221a60e6f0b6eabc343e061d4994e6048f984fb
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2020-04-04 18:43:39 +0100

    Feat(Exception): Improve Exception Handling

--- src/Exception/TranslationException.php
+++ src/Exception/TranslationException.php
@@ -2,8 +2,24 @@
 
 namespace Tuforti\MoneyToWords\Exception;
 
+use Throwable;
 use Exception;
 
 class TranslationException extends Exception
 {
+    protected $metadata;
+
+    function __construct($message, $code = 0, $metadata = [], Throwable $previous = null)
+    {
+        parent::__construct($message, $code = $code, $previous);
+        $this->metadata = $metadata;
+    }
+
+    /**
+     * Get exception metadata.
+     */
+    public function getMetaData()
+    {
+        return $this->metadata;
+    }
 }
