120de87cddf94e31dcc91e0608e2306fec8fa9ec
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2020-07-11 00:20:35 +0100

    Chore(Example): Update Example With Chinese Numeral

--- example/src/Example.php
+++ example/src/Example.php
@@ -3,13 +3,20 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Tuforti\MoneyToWords\Converter;
+use Tuforti\MoneyToWords\Contracts\Cache;
 
-$converter = new Converter("naira", "kobo");
+$converter = new Converter(
+    "AIzaSyDT42-Onn3pQM8JnSakvYlhsKR4WCzG47Y",
+    "naira",
+    "kobo",
+    $languageTo = "en",
+    $cache = Cache::class
+);
 
-echo ($converter->convert(374));
-echo ($converter->convert(23.45));
-echo ($converter->convert(748247284782));
-echo ($converter->convert(748247284782.34));
-echo ($converter->convert('34'));
-echo ($converter->convert('2345.34'));
-echo ($converter->convert('3453345'));
+var_dump($converter->convert("八百七十二万七千八百二十四"));
+var_dump($converter->convert(23.45));
+var_dump($converter->convert(748247284782));
+var_dump($converter->convert(748247284782.34));
+var_dump($converter->convert('34'));
+var_dump($converter->convert('2345.34'));
+var_dump($converter->convert('3453345'));
